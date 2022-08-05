bdf9df26eb95e88830a93e0bb0a3ddf2bef7bbac
EsnaiderRey <esnaiderre@gmail.com>
2022-08-04 23:22:45 -0500

    add the menu mobile deploy fucion with the index.html

--- main.js
+++ main.js
@@ -1,8 +1,16 @@
 const navBarEmail = document.querySelector(".navbar-email");
 const desktopMenu = document.querySelector(".desktop-menu");
+const mobileMenuBotton = document.querySelector(".menu");
+const mobileMenu = document.querySelector(".mobile-menu");
+
 
 navBarEmail.addEventListener("click", toggleDesktopMenuEmail);
+mobileMenuBotton.addEventListener("click", toggleMobileMenu);
+
 
 function toggleDesktopMenuEmail() {
     desktopMenu.classList.toggle("inactive")
+}
+function toggleMobileMenu(){
+    mobileMenu.classList.toggle("inactive")
 }
